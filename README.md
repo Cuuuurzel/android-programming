ab7e51a7ef85b676830caecb517d2f8d5cfc82d0
Cuuuurzel <fcurzel04@gmail.com>
2013-04-19 18:42:52 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+android-programming
+===================
+
+An introduction to android programming using python-kivy, and comparison with java.
---
+++ res/SimpleCalc/java/SimpleCalc/gen/com/cuuuurzel/simplecalc/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cuuuurzel.simplecalc;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
